f72401b9468487b4cf9e731722bb87ee68a4f2e0
Mendrika-Kamel <mendrika-kamel.randrianarisoa@univ-tlse3.fr>
2024-09-30 09:35:23 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,6 +4,9 @@ public class Romain {
 	
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
+	
 	
 	
 	public Romain(String nom, int force) {
@@ -45,10 +48,45 @@ public class Romain {
 		
 	}
 	
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2 : 
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1 : 
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat "+nom+" possède déjà un "+equipement+" !");
+			} else {
+				ajoutEquipement(equipement);
+
+			}
+			break;
+		case 0 : 
+			ajoutEquipement(equipement);
+			break;
+			
+		}
+	}
+	
+	private void ajoutEquipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		System.out.println("Le soldat "+nom+"  s’équipe avec un "+equipement+" !");
+		nbEquipement++;
+		
+	}
+	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus",6);
 		minus.parler("Hey, je suis Minus, un romain.");
 		minus.recevoirCoup(2);
+		Equipement a = Equipement.BOUCLIER;
+		Equipement b = Equipement.CASQUE;
+				
+		minus.sEquiper(b);
+		minus.sEquiper(b);
+		minus.sEquiper(a);
+		minus.sEquiper(b);
+		
 	}
 
 }
